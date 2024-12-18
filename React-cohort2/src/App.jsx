b4fb3155fe0b425089d388855e0c4e61783140fc
Abhishek <abhipro01@gmail.com>
2024-12-18 20:39:33 +0530

    Add Tailwind Styles

--- React-cohort2/src/App.jsx
+++ React-cohort2/src/App.jsx
@@ -4,16 +4,23 @@ function App() {
   const [count, setCount] = useState(0);
 
   const handleAdd = () => setCount((c) => c + 1);
-  const handleReset = () => setCount((c) => c = 0);
+  const handleReset = () => setCount((c) => (c = 0));
   const handleSub = () => setCount((c) => c - 1);
   return (
     <>
-      <div>{count}</div>
-      <button onClick={handleAdd}>Add</button>
-      <button onClick={handleReset}>Reset</button>
-      <button onClick={handleSub}>Sub</button>
+      <div className="h-screen w-full bg-gray-[#2F3C7E]">
+        <div className="flex justify-center items-center h-full flex-col">
+        <div className="text-8xl font-semibold">{count}</div>
+        <div className="mt-8">
+        <button className="mx-4 text-4xl font-bold rounded-xl bg-[#2F3C7E] text-[#FBEAEB]  px-4 py-4" onClick={handleAdd}>Add</button>
+        <button className="mx-4 text-4xl font-bold rounded-xl bg-[#2F3C7E] text-[#FBEAEB]  px-4 py-4" onClick={handleReset}>Reset</button>
+        <button  className="mx-4 text-4xl font-bold rounded-xl bg-[#2F3C7E] text-[#FBEAEB] px-4 py-4" onClick={handleSub}>Sub</button>
+        </div>
+        </div>
+      </div>
     </>
   );
 }
+
 
 export default App;
