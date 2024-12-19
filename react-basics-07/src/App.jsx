84b9fd4d6774662ff810f4717cf1f768ff3c5507
Abhishek <abhipro01@gmail.com>
2024-12-19 21:58:25 +0530

    conditional rendering

---
+++ react-basics-07/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const Validpassword = () => <h1>Valid Password</h1>
+const InvalidPassword = ()=> <h1>Invalid Passowrd</h1>
+
+const Password = ({isValid}) => {
+  if(isValid){
+    return <Validpassword />
+  }
+  return <InvalidPassword />
+}
+
+function App() {
+  const [valid,setValid] = useState(false)
+
+  return (
+    <>
+     <Password isValid={false}/>
+    </>
+  )
+}
+
+export default App
