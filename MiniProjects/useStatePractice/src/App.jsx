da2882538d2c56a37359eab4b727e5b4866b560c
Abhishek <abhipro01@gmail.com>
2024-12-20 21:35:23 +0530

    useState Advance function return default value

--- MiniProjects/useStatePractice/src/App.jsx
+++ MiniProjects/useStatePractice/src/App.jsx
@@ -1,17 +1,43 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [friends,setFriends] = useState(["jackie","chan","John", "wick"])
+  // const [friends,setFriends] = useState(["jackie","chan","John", "wick"])
+  // const [movie,setMovie] = useState({
+  //   title:"John wick",
+  //   Rating:9
+  // })
+
+  // const updateRating = () => setMovie({...movie,Rating : 8})
+
+  // const handleAdd = () => setFriends([...friends,"newFriend"])
+  // const handleRemove = ()=> setFriends(friends.filter(f => f!=="newFriend"))
+  // const handleUpdate = ()=> setFriends(friends.map(f => f ==="newFriend" ? "Nice Name " : f))
+
+  const [count , setCount] = useState(()=>{
+    const initialValue = 10;
+    return initialValue;
+  })
+
+  const [randomNumber,setRandomNumber] = useState(()=>{
+    const random = Math.floor(Math.random()*100)
+    return random
+  })
+
+  const [fname,setFname] = useState(()=>{
+    const name = localStorage.getItem('name');
+    return name ? JSON.parse(name) : ""
+  })
+
+  useEffect(()=>{
+    localStorage.setItem("name",JSON.stringify(fname))
+  },[fname])
 
 
-  const handleAdd = () => setFriends([...friends,"newFriend"])
-  const handleRemove = ()=> setFriends(friends.filter(f => f!=="newFriend"))
-  const handleUpdate = ()=> setFriends(friends.map(f => f ==="newFriend" ? "Nice Name " : f))
 
   return (
     <>
-      <div>
+      {/* <div>
       <ul>
 
       {friends.map((f) => (
@@ -25,7 +51,42 @@ function App() {
       <button onClick={handleRemove}>Remove Friends</button>
       <button onClick={handleUpdate}>Update Friends</button>
       </div>
+      <div>
+        <p>Title : {movie.title}</p>
+        <p>Rating :{movie.Rating}</p>
+        <button onClick={updateRating}>Update Rating.</button>
+      </div> */}
+        {/*-------------------------------------------------------- USE OF FUNCTIONS IN USE STATE ------------------------------------ */}
 
+
+        <div>
+        <h1>Count : {count}</h1>
+        <button onClick={()=> {
+          setCount(count+1)
+        }} > INC </button>
+        <button onClick={()=>{
+          setCount(count - 1)
+        }} > DEC</button>
+        </div>
+
+
+
+        <div>
+          <h2>Random Number : {randomNumber}</h2>
+          <button onClick={()=>{
+            const r = Math.floor(Math.random()*100);
+            setRandomNumber(r);
+          }}>Generate Random Number</button>
+        </div>
+    
+          <div>
+            <h3>Enter a Name : {fname}</h3>
+            <input placeholder='Enter your name' value={fname} onChange={(e)=>{
+              setFname(e.target.value)
+            }}></input>
+          </div>
+    
+    
     </>
   )
 }
