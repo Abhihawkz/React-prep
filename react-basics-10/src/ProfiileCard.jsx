9c7b24d5245d5b3470401695d1b3356d5713e34d
Abhishek <abhipro01@gmail.com>
2024-12-20 20:19:02 +0530

    Added Styles inline styles

---
+++ react-basics-10/src/ProfiileCard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const ProfiileCard = () => {
+  const styles = {
+    backgroundColor :"lightgrey",
+    color:"black",
+    padding:'10px',
+    borderRadius:'10px',
+    marginTop:'10px'
+  }
+
+  return (
+    <div style={styles}>
+      <h1>Hello User!!!</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        recusandae quibusdam quia. Quia esse quasi amet, ducimus nam, magnam
+        veritatis, iste alias error quaerat optio facilis illum necessitatibus
+        sequi ex.
+      </p>
+    </div>
+  );
+};
+
+export default ProfiileCard;
---
+++ react-basics-10/src/StyleCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const StyleCard = () => {
+  return (
+    <div style={{backgroundColor: 'lightblue',padding:'10px',color:'darkblue',borderRadius:'10px'}}>
+      <h1>Style Card</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia nam
+        aperiam harum fuga ea veritatis maxime mollitia minus! Deleniti quisquam
+        explicabo totam debitis assumenda omnis veniam incidunt harum atque
+        ratione.
+      </p>
+    </div>
+  );
+};
+
+export default StyleCard;
