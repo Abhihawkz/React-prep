5f14e91ffa892cc0af228bb2ab5596dbe830242d
Abhishek <abhipro01@gmail.com>
2024-12-21 17:53:34 +0530

    Fetched Meals details from api and created a Meals card

---
+++ MiniProjects/Card/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
+import './styles.css';
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+      .then((res) => {
+        console.log(res.data.meals);
+        setItems(res.data.meals);
+      });
+  }, []);
+
+  const itemslist = items.map(({ strMeal, strMealThumb, idMeal }) => {
+    return (
+      <section className="card">
+        <img src={strMealThumb} />
+        <section className="content">
+          <p>{strMeal}</p>
+          <p>#{idMeal}</p>
+        </section>
+      </section>
+    );
+  });
+  return <div className="items-container">{itemslist}</div>;
+}
+
+export default App;
